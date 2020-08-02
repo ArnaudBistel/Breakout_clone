c008f47b8cbe3a835d7a7b19e01d507e4e2e659e
ArnaudBistel <40674928+ArnaudBistel@users.noreply.github.com>
2020-08-02 15:15:12 +0200

    game manager, meshes, materials, model

---
+++ Scripts/Ball.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private float _speed = 20f;
+    private Vector3 _velocity;
+    private Rigidbody _rigidbody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.velocity = Vector3.down * _speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.velocity = _rigidbody.velocity.normalized * _speed;
+        _velocity = _rigidbody.velocity;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        _rigidbody.velocity = Vector3.Reflect(_velocity, collision.contacts[0].normal);
+    }
+}
